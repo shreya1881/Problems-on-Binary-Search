682fa26cc274daf0960a53baa4bd18deb392d8fa
Shreya Dutta <85094341+shreya1881@users.noreply.github.com>
2023-03-16 20:55:26 +0530

    Create Aggressive Cows.cpp

---
+++ Aggressive Cows.cpp
@@ -0,0 +1,43 @@
+bool isPossiblesol(vector<int> &stalls, int k, int mid){
+    int cowcount=1;
+    int lastpos = stalls[0];
+
+    for(int i=0; i<stalls.size(); i++){
+        if(stalls[i]-lastpos>=mid){
+            cowcount++;
+            if(cowcount==k)
+            return true;
+            lastpos=stalls[i];
+        }
+
+    }
+    return false;
+}
+
+
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    //    Write your code here.
+    sort(stalls.begin(),stalls.end());
+    int s=0;
+    int maxi=-1;
+    for(int i=0; i<stalls.size();i++)
+    maxi=max(maxi,stalls[i]);
+    int e = maxi;
+    int mid= s+(e-s)/2;
+
+    int ans=-1; 
+
+    while(s<=e){
+      if (isPossiblesol(stalls, k, mid)) {
+        ans = mid;
+        s = mid + 1;
+      }
+      else{
+          e=mid-1;
+      }
+      mid = s+(e-s)/2;
+    }
+    return ans;
+}
